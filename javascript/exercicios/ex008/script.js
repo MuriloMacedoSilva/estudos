d5ce36ce64d4373a84eab8cf2d36b9362c7262d1
MuriloMacedoSilva <152343609+MuriloMacedoSilva@users.noreply.github.com>
2024-01-17 16:00:43 -0300

    Update script.js

--- javascript/exercicios/ex008/script.js
+++ javascript/exercicios/ex008/script.js
@@ -1,7 +1,7 @@
 var vel = 60;
 
 console.log(`A velocidade do seu carro é ${vel}km/h`);
-if (vel > 60) {
+if (vel > 60) { //condição simples
   console.log("voce ultrapassou a velocidade permitida, MULTADO!");
 }
 
